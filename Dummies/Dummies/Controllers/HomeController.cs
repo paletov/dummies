4e5031f4109dfb218c719005baf244fd65c6024e
dimo <dimobskn@gmail.com>
2013-04-13 21:09:18 +0300

    Add search and business profile

--- Dummies/Dummies/Controllers/HomeController.cs
+++ Dummies/Dummies/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dummies.Filters;
 
 namespace Dummies.Controllers
 {
+    [Authorize]
+    [InitializeSimpleMembership]
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
--- Dummies/Dummies/Controllers/StudentController.cs
+++ Dummies/Dummies/Controllers/StudentController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dummies.Filters;
+using WebMatrix.WebData;
 
 namespace Dummies.Controllers
 {
+    [Authorize]
+    [InitializeSimpleMembership]
     public class StudentController : Controller
     {
         //
@@ -13,8 +17,33 @@ namespace Dummies.Controllers
 
         public ActionResult Index()
         {
+            //---------------------------MOCKUP
+            if (WebSecurity.CurrentUserName == "bigboss")
+            {
+                return RedirectToAction("BusinessProfile");
+            }
+            //-----------------------------
             return View();
         }
+
+        //-------------------------------MOCKUP
+        public ActionResult BusinessProfile()
+        {
+            return View();
+        }
+
+        public ActionResult JavaDeveloper()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+        //----------------------------------
+
+
 
     }
 }
